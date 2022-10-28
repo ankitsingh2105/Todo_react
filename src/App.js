6be045bd74e8f0a6088f8a23fcbcba52733bff1b
Ankit Singh Chauhan <88767069+ankitsingh2105@users.noreply.github.com>
2022-10-28 17:21:12 +0530

    Added Edit Button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ export default function App() {
   const [array, setArray] = useState(
     JSON.parse(localStorage.getItem("notes")) ?? []
   );
+  let newIndex;
   useEffect(() => {
+    one.current.value = "";
     localStorage.setItem("notes", JSON.stringify(array));
   }, [array]);
   const Add = () => {
@@ -16,9 +18,35 @@ export default function App() {
   const deleting = (e) => {
     setArray((e1) => {
       return e1.filter((e2, index) => {
-        return index !== e.index - 1; // here
+        return index !== e.index - 1;
       });
     });
+  };
+  const editing = (e) => {
+    newIndex = e.index;
+    console.log("haloo->  ", e);
+    let newvalue;
+    array.map((e1, index) => {
+      if (index + 1 === e.index) {
+        newvalue = e1;
+      }
+      return e1;
+    });
+    console.log("newArray -< ", newvalue);
+    one.current.value = newvalue;
+  };
+  const save = () => {
+    let info = one.current.value;
+    console.log(newIndex, " and ", info);
+    console.log("this si array element -> ", array[newIndex - 1]);
+    array[newIndex - 1] = info;
+    if (newIndex !== undefined) {
+      setArray((e) => {
+        let temp = [...e];
+        temp[newIndex - 1] = info;
+        return temp;
+      });
+    }
   };
   const one = useRef(null);
   return (
@@ -26,7 +54,10 @@ export default function App() {
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <div className="align">
-          <input ref={one} />
+          <input ref={one} />{" "}
+          <button onClick={save} className="save">
+            Save
+          </button>
           <br />
           <br />
           <button onClick={Add}>Add</button>
@@ -34,7 +65,13 @@ export default function App() {
         <div className="align">
           {array.map((e, index) => {
             return (
-              <Ank key={index} onSelect={deleting} index={index + 1} name={e} />
+              <Ank
+                key={index}
+                edit={editing}
+                onSelect={deleting}
+                index={index + 1}
+                name={e}
+              />
             );
           })}
         </div>
