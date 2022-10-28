f3fe4a365a5a6d398b41d21763e3c953ef41880c
Ankit Singh Chauhan <88767069+ankitsingh2105@users.noreply.github.com>
2022-10-28 17:21:42 +0530

    Added edit button

--- src/Ank.jsx
+++ src/Ank.jsx
@@ -1,17 +1,19 @@
 export default function Ank(props) {
-  const { name, index, onSelect } = props;
+  const { name, index, onSelect, edit } = props;
   return (
     <div className="align1">
       <div className="one1">{index}.</div>
       <div className="one2">{name}</div>
       <button
         className="delete"
-        onClick={() => {
+        onClick={function () {
           onSelect({ index });
         }}
       >
         Delete
-      </button>
+      </button>{" "}
+      &nbsp;&nbsp;
+      <button onClick={() => edit({ index })}>Edit</button>
     </div>
   );
 }
